05eab32a9698c79404460cf7399b0d429629db09
Gozals25 <shailentcer@gmail.com>
2016-11-08 07:41:25 +0700

    model events

--- app/Author.php
+++ app/Author.php
@@ -5,6 +5,7 @@ namespace App;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Session;
 
 class Author extends Model
 {
@@ -12,6 +13,33 @@ class Author extends Model
     use Sluggable;
 
     protected $dates = ['deleted_at'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::deleting(function($author) {
+            // mengecek apakah penulis masih punya buku
+            if ($author->books->count() > 0) {
+                // menyiapkan pesan error
+                $html = 'Penulis tidak bisa dihapus karena masih memiliki buku : ';
+                $html .= '<ul>';
+                foreach ($author->books as $book) {
+                    $html .= "<li>$book->title</li>";
+                }
+                $html .= '</ul>';
+
+                Session::flash("flash_notification", [
+                    "level"=>"danger",
+                    "message"=>$html
+                ]);
+
+                // membatalkan proses penghapusan
+                return false;
+            }
+        });
+    }
+
     /**
      * Return the sluggable configuration array for this model.
      *
