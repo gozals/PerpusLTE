b580997de7af509c7bb3d4bde42a2244bff7b8e6
Gozals25 <shailentcer@gmail.com>
2016-11-10 00:26:24 +0700

    add missing code for change password

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('passcheck', function ($attribute, $value, $parameters) {
+            return Hash::check($value, $parameters[0]);
+        });
     }
 
     /**
